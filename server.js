fc7bc7bc6591a2a6e4c20646b9e62329e4eb87b7
Pablo Gonzalez <pablo_igncio18@hotmail.cl>
2020-08-07 15:19:27 -0400

    Primero

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+const Discord = require('discord.js');
+
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Estoy listo perras');
+});
+
+
+client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === '!Server') {
+      // Send "pong" to the same channel
+      message.channel.send('https://www.youtube.com/');
+      
+    }
+  });
+
+
+client.login(process.env.token);
